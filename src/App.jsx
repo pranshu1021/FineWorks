3b6beb79c39f3bd2e787387e16594aea5381befc
Pranshu Gupta <pranshugupta1423@gmail.com>
2026-06-17 20:04:13 +0530

    5th update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Navbar from "./Navbar.jsx";
 
-function App(){
-  return <h1>FineWorks</h1>;
+function App() {
+  return (
+    <div>
+      <h1>FineWorks</h1>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,6 @@
+import "./Navbar.css"
+
+export default function navbar(){
+<div>Hello</div>
+}
+
